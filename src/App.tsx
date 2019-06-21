0d70bac28ae6e205443cad28ed7aeeb6fb052076
Sajjad Hashemian <wolaws@gmail.com>
2019-06-21 06:34:37 +0430

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 import './App.css';
 
-const App: React.FC = () => {
+export function App() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column h-100">
+      <main className="flex-shrink-0" role="main">
+        <div className="container">
+          <h1 className="mt-3">{'آپلود فایل'}</h1>
+          <div {...getRootProps({ className: 'dropzone' })}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <span>{'فایل‌ها را اینجا رها کنید...'}</span>
+            ) : (
+              <span>
+                {'فایل‌ها را اینجا رها کنید یا برای انتخاب اینجا کلیک کنید'}
+              </span>
+            )}
+          </div>
+        </div>
+      </main>
+      <footer className="footer mt-auto py-3">
+        <div className="container">
+          <span>{'قدرت گرفته از '}</span>
+          <a href="https://transfer.sh/">{'transfer.sh'}</a>
+        </div>
+      </footer>
     </div>
   );
 }
-
-export default App;
